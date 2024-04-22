6707263c13499b023efdd997437a20cd2b9ddb3d
aalexandramarta <r0972487@student.thomasmore.be>
2024-04-22 16:34:13 +0200

    nnn

--- collin.cs
+++ collin.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace CharityApplication
 {
     class collin
     {
+        Console.Write("hello)
     }
 }
