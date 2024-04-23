8412c29db2d8a5190bca8834aab54f85c97840cd
Daryna Denysenko <darinadenisenko2204@gmail.com>
2024-04-23 13:03:02 +0200

    check

--- obj/Debug/net6.0-windows/CharityApplication.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/CharityApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharityApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a791e3cca462875be5411bdfb23ed7dc36d446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharityApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharityApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
