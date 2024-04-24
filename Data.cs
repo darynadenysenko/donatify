2887ff65d561d63b1257287011ec9577bfda9d7b
aalexandramarta <r0972487@student.thomasmore.be>
2024-04-24 14:25:38 +0200

    mysqlconnector installed, data.cs created

---
+++ Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySqlConnector;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityApplication
+{
+    public class Data
+    {
+
+    }
+}
