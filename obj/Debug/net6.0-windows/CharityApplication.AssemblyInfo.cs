100ee266504a9e899bd953e7a3a9e9bb04a63b18
aalexandramarta <r0972487@student.thomasmore.be>
2024-04-24 11:00:38 +0200

    nn

--- obj/Debug/net6.0-windows/CharityApplication.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/CharityApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharityApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0050521450b2deec33c00ab86f93a208e4345485")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharityApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharityApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
