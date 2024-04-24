4d683d9df9efa6c334c6f8f39784142b6137f598
aalexandramarta <r0972487@student.thomasmore.be>
2024-04-24 14:28:06 +0200

    data.cs connected to database

--- Data.cs
+++ Data.cs
@@ -9,6 +9,31 @@ namespace CharityApplication
 {
     public class Data
     {
+        private string connectionString = "datasource=127.0.0.1; port=3306; username = root; password=; database=donatify;";
 
+       
+
+        //opening a connection
+        private int Insert(string query) 
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, connection);
+            
+
+            try
+            {
+                connection.Open();
+                int result = commandDatabase.ExecuteNonQuery();
+
+                return (int)commandDatabase.LastInsertedId;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return -1; //closing the connection
+        }
     }
 }
