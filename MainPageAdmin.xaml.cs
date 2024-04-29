33c80a2c1d53e5fdd4dcc20aa408380cbfd4531e
aalexandramarta <r0972487@student.thomasmore.be>
2024-04-29 16:28:49 +0200

    main page admin and login button connected

---
+++ MainPageAdmin.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CharityApplication
+{
+    /// <summary>
+    /// Interaction logic for MainPageAdmin.xaml
+    /// </summary>
+    public partial class MainPageAdmin : Page
+    {
+        public MainPageAdmin()
+        {
+            InitializeComponent();
+        }
+        private void ListOfOrganizations_Click(object sender, RoutedEventArgs e)
+        {
+            
+            mainAdminFrame.Navigate(new Uri("ListOfOrganizationsAdmin.xaml", UriKind.Relative));
+        }
+        private void ListOfEvents_Click(object sender, RoutedEventArgs e)
+        {
+
+            mainAdminFrame.Navigate(new Uri("ListOfEventsAdmin.xaml", UriKind.Relative));
+        }
+        private void ListOfUsers_Click(object sender, RoutedEventArgs e)
+        {
+
+            mainAdminFrame.Navigate(new Uri("ListOfUsersAdmin.xaml", UriKind.Relative));
+        }
+    }
+}
