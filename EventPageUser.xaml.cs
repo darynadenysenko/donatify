f8172a91671e440f7d4e8d4ec489f47d186c9d5a
aalexandramarta <r0972487@student.thomasmore.be>
2024-05-21 11:57:43 +0200

    hh

--- EventPageUser.xaml.cs
+++ EventPageUser.xaml.cs
@@ -43,49 +43,52 @@ namespace CharityApplication
             Data dataAccess = new Data();
             List<Event> events = dataAccess.GetAllEvents();
 
-
+            DateTime currentDate = DateTime.Now;
 
             // Create a Label with a Button for each event and add it to the StackPanel
             foreach (var evt in events)
             {
-                StackPanel eventContainer = new StackPanel();
-                eventContainer.Orientation = Orientation.Vertical;
-                eventContainer.HorizontalAlignment = HorizontalAlignment.Stretch;
-                eventContainer.VerticalAlignment = VerticalAlignment.Stretch;
+                if (evt.EndDate <= currentDate)
+                {
+                    StackPanel eventContainer = new StackPanel();
+                    eventContainer.Orientation = Orientation.Vertical;
+                    eventContainer.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    eventContainer.VerticalAlignment = VerticalAlignment.Stretch;
 
-                // Create and configure the TextBlock
-                TextBlock textBlock = new TextBlock();
-                textBlock.Background = Brushes.White;
-                textBlock.Margin = new Thickness(10, 0, 10, 0);
-                textBlock.Height = 100;
-                textBlock.Text = evt.Name + "\n\n" + evt.StartDate + "-" + evt.EndDate + "\n\n" + evt.Description;
-                textBlock.TextWrapping = TextWrapping.Wrap;
-                textBlock.Width = 100; // Allow the TextBlock to stretch horizontally
-                textBlock.HorizontalAlignment = HorizontalAlignment.Stretch; // Allow the TextBlock to stretch horizontally
-                textBlock.Height = double.NaN; // Allow the TextBlock to stretch vertically
-                textBlock.VerticalAlignment = VerticalAlignment.Stretch;
-                textBlock.FontFamily = new FontFamily(new Uri("pack://application:,,,/CharityApplication;component/"), "./Font/#Julius Sans One");
-                textBlock.TextAlignment = TextAlignment.Center;
+                    // Create and configure the TextBlock
+                    TextBlock textBlock = new TextBlock();
+                    textBlock.Background = Brushes.White;
+                    textBlock.Margin = new Thickness(10, 0, 10, 0);
+                    textBlock.Height = 100;
+                    textBlock.Text = evt.Name + "\n\n" + evt.StartDate + "-" + evt.EndDate + "\n\n" + evt.Description + "\n\n" + evt.CurrentAmountRaised;
+                    textBlock.TextWrapping = TextWrapping.Wrap;
+                    textBlock.Width = 100; // Allow the TextBlock to stretch horizontally
+                    textBlock.HorizontalAlignment = HorizontalAlignment.Stretch; // Allow the TextBlock to stretch horizontally
+                    textBlock.Height = double.NaN; // Allow the TextBlock to stretch vertically
+                    textBlock.VerticalAlignment = VerticalAlignment.Stretch;
+                    textBlock.FontFamily = new FontFamily(new Uri("pack://application:,,,/CharityApplication;component/"), "./Font/#Julius Sans One");
+                    textBlock.TextAlignment = TextAlignment.Center;
 
-                // Create and configure the Button
-                Button button = new Button();
-                button.Content = "Donate";
-                button.Click += (sender, e) => ShowDonatePage(evt); // Pass the event to the click event handler
-                button.Background = Brushes.White;
-                button.FontFamily = new FontFamily(new Uri("pack://application:,,,/CharityApplication;component/"), "./Font/#Julius Sans One");
-                button.FontSize = 18;
-                button.Height = 33;
-                button.Width = 100;
-                button.BorderBrush = Brushes.Transparent;
-                button.HorizontalContentAlignment = HorizontalAlignment.Center;
-                button.VerticalContentAlignment = VerticalAlignment.Center;
+                    // Create and configure the Button
+                    Button button = new Button();
+                    button.Content = "Donate";
+                    button.Click += (sender, e) => ShowDonatePage(evt); // Pass the event to the click event handler
+                    button.Background = Brushes.White;
+                    button.FontFamily = new FontFamily(new Uri("pack://application:,,,/CharityApplication;component/"), "./Font/#Julius Sans One");
+                    button.FontSize = 18;
+                    button.Height = 33;
+                    button.Width = 100;
+                    button.BorderBrush = Brushes.Transparent;
+                    button.HorizontalContentAlignment = HorizontalAlignment.Center;
+                    button.VerticalContentAlignment = VerticalAlignment.Center;
 
-                // Add the TextBlock and Button to the container
-                eventContainer.Children.Add(textBlock);
-                eventContainer.Children.Add(button);
+                    // Add the TextBlock and Button to the container
+                    eventContainer.Children.Add(textBlock);
+                    eventContainer.Children.Add(button);
 
-                // Add the container (containing TextBlock and Button) to the eventsStackPanel
-                eventsStackPanel.Children.Add(eventContainer);
+                    // Add the container (containing TextBlock and Button) to the eventsStackPanel
+                    eventsStackPanel.Children.Add(eventContainer);
+                }
             }
         }
 
